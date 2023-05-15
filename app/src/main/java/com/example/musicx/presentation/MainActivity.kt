ee9ba19cf6a899e6691d0438dfdb66dac0a14ad1
Gautam <gautamhazarika01@gmail.com>
2023-05-16 02:02:43 +0530

    Permission Request Enabled

--- app/src/main/java/com/example/musicx/presentation/MainActivity.kt
+++ app/src/main/java/com/example/musicx/presentation/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.musicx.presentation
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.example.musicx.presentation.common.MainViewModel
 import com.example.musicx.presentation.common.PlayerCardEvent
 import com.example.musicx.presentation.util.SetupNavigation
@@ -19,35 +30,71 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var mainViewModel: MainViewModel
 
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+    private val permission = mutableStateOf(false)
+
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
+        permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) {
+            when {
+                it.getOrDefault(Manifest.permission.READ_MEDIA_AUDIO, false) -> {
+                    permission.value = true
+                }
+            }
+
+        }
+
+        permissionLauncher.launch(
+            arrayOf(
+                Manifest.permission.READ_MEDIA_AUDIO, Manifest.permission.POST_NOTIFICATIONS
+            )
+        )
+
+
+
+
         super.onCreate(savedInstanceState)
         setContent {
             MusicXTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
 
-                    SetupNavigation(
-                        mainViewModel.playerCardState.value.currentPlayingAudio,
-                        mainViewModel.playerCardState.value.isPlaying,
-                        {
-                            mainViewModel.onEvent(PlayerCardEvent.OnNextClicked)
-                        },
-                        {
-                            mainViewModel.onEvent(PlayerCardEvent.OnPreviousClicked)
-                        },
-                        {
-                            mainViewModel.onEvent(PlayerCardEvent.OnTogglePlayClicked)
+                    if (permission.value) {
+                        SetupNavigation(
+                            mainViewModel.playerCardState.value.currentPlayingAudio,
+                            mainViewModel.playerCardState.value.isPlaying,
+                            {
+                                mainViewModel.onEvent(PlayerCardEvent.OnNextClicked)
+                            },
+                            {
+                                mainViewModel.onEvent(PlayerCardEvent.OnPreviousClicked)
+                            },
+                            {
+                                mainViewModel.onEvent(PlayerCardEvent.OnTogglePlayClicked)
 
-                        },
-                        {
-                            mainViewModel.onEvent(PlayerCardEvent.PlayTrack(it))
-                        },
+                            },
+                            {
+                                mainViewModel.onEvent(PlayerCardEvent.PlayTrack(it))
+                            },
+                        )
+                    } else {
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                text = "Please grant all permissions.",
+                                color = Color.Black.copy(0.5f)
+                            )
+                        }
+                    }
 
-
-                    )
                 }
             }
         }
